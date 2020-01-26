7890d948d3b804295aad73b6e031931e83de35a6
Marcin Delektowski <marcin.delektowski@gmail.com>
2020-01-26 23:31:01 +0100

    Add leave time and change script

--- makephoto.sh
+++ makephoto.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
- fswebcam -r 160x120 --loop 2 lightSourceImage.jpg
+ fswebcam -r 160x120 --loop 2 /home/pi/IsToiletBusy/lightSourceImage.jpg &
+ sleep 2
+ http-server /home/pi/IsToiletBusy 
